e0c83838598742a6c98876c98e709f5bfb724f55
Rialgou <44506529+Rialgou@users.noreply.github.com>
2020-06-08 01:53:29 -0400

    Update Predictor.cpp

--- src/Predictor/Predictor.cpp
+++ src/Predictor/Predictor.cpp
@@ -12,17 +12,26 @@ Predictor::Predictor(){
 Predictor::~Predictor(){
 	delete t;
 }
+//funcion recursiva para imprimir coincidencias con el prefijo especificado
 void Predictor::suggestions(string prefix){
-	
+	//se comprueba si es el final de la palabra y se imprime en caso de que corresponda al prefijo
 	if(this->root->c==EOS){
 		cout<<prefix<<endl;
 	}
+	//en caso de que no queden nodos hijos correspondientes al prefijo se termina el metodo
 	if(t->isLastNode()){
 		return;
 	}
+	//
 	for(int i=0;i<NUM_LETTERS;i++){
+		//en caso de haber un hijo en la posicion i
 		if(this->root->children[i]){
-			
+			//se agrega el caracter correspondiente del hijo, en base al codigo ASCII
+			prefix.push_back(97+i);
+			//se recurre nuevamente a la función
+			suggestions(this->root->children[i]);
+			//se elimina el ultimo caracter
+			prefix.pop_back();
 		}
 	}
 }
