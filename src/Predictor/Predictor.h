70def3250ce50a7c6aebebefcd6c49171fe285d6
Rialgou <44506529+Rialgou@users.noreply.github.com>
2020-06-09 00:40:25 -0400

    Update Predictor.h

--- src/Predictor/Predictor.h
+++ src/Predictor/Predictor.h
@@ -1,9 +1,16 @@
 #include "../Trie/Trie.h"
+<<<<<<< HEAD
 #include <bits/stdc++.h>
+=======
+
+#include <bits/stdc++.h>
+
+>>>>>>> aa9e7a9c6f6151e19d7351bdb24428bac6d8dba6
 class Predictor{
 	private:
 		struct TrieNode* root;
 		Trie *t;
+		int aux;
 		std::vector<std::pair<int,std::string>> v;
 		int aux;
 	public:
