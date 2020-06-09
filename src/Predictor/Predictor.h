5ecbfc991ea57ce00f7163efb97141a48e9be6a2
Rialgou <44506529+Rialgou@users.noreply.github.com>
2020-06-08 21:09:12 -0400

    Update Predictor.h

--- src/Predictor/Predictor.h
+++ src/Predictor/Predictor.h
@@ -4,8 +4,10 @@ class Predictor{
 	private:
 		struct TrieNode* root;
 		Trie *t;
+
 	public:
 		Predictor(Trie* t);
 		~Predictor();
-		void suggestions(std::string prefix);
+		void suggestions(std::string prefix,int k);
+		int printSuggestions(std::string prefix,int k);
 };
