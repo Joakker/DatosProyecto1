70a531e61fc2945c2e01d761527dcfa0675498e3
Rialgou <44506529+Rialgou@users.noreply.github.com>
2020-06-09 00:40:54 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "Controlador/Controlador.h"
   
 int main(int argc, char* argv[]) {
-    if (argc == 3) {
+    if (argc == 5) {
         Controlador c(argv[1]);
-        c.buscarCadenas(argv[2], "", 1);
+        c.buscarCadenas(argv[2], argv[3], std::stoi(argv[4]));
     }
     return 0; 
 } 
