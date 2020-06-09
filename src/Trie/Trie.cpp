0e307c469bb950e0994b7b2e4248d71da54bce77
Rialgou <44506529+Rialgou@users.noreply.github.com>
2020-06-08 21:54:06 -0400

    Update Trie.cpp

--- src/Trie/Trie.cpp
+++ src/Trie/Trie.cpp
@@ -66,7 +66,9 @@ bool Trie::isLastNode(struct TrieNode* root){
     }
     return false;
 }
-
+int getFrequency(){
+    return frequency;
+}
 struct TrieNode* Trie::getNode(){
     return this->root;
 }
